cece4a32dc2d347b74f487975628aaae43797163
Shivam Meena <meenashivam9650@gmail.com>
2023-02-03 12:15:10 +0530

    no default theme

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -11,7 +11,7 @@ export const handle = (async ({ event, resolve }) => {
 	}
 	if (theme) {
 		return resolve(event, {
-			transformPageChunk: ({ html }) => html.replace('data-theme="winter"', `data-theme="${theme}"`)
+			transformPageChunk: ({ html }) => html.replace('data-theme=""', `data-theme="${theme}"`)
 		});
 	}
 	return resolve(event);
